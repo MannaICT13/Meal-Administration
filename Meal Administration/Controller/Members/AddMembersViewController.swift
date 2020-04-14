b64ab3eb85dcfc77ba1ee669d3fef87e39c001fb
Md Khaled Hasan Manna <khmannaict13@gmail.com>
2020-04-14 21:29:03 +0600

    Created outlets and design UI for AddMembersViewController

---
+++ Meal Administration/Controller/Members/AddMembersViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AddMembersViewController.swift
+//  Meal Administration
+//
+//  Created by Md Khaled Hasan Manna on 14/4/20.
+//  Copyright © 2020 Md Khaled Hasan Manna. All rights reserved.
+//
+
+import UIKit
+
+class AddMembersViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    //textField Outlets
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var rentTextField: UITextField!
+    @IBOutlet weak var netTextField: UITextField!
+    @IBOutlet weak var gassTextField: UITextField!
+    @IBOutlet weak var khalaTextField: UITextField!
+    @IBOutlet weak var currentTextField: UITextField!
+    @IBOutlet weak var waterTextField: UITextField!
+    @IBOutlet weak var othersTextField: UITextField!
+    
+    //Button Outlets
+    @IBOutlet weak var saveBtnOutlets: UIButton!
+    
+    //icon Image Outlets
+    @IBOutlet weak var nameImg: UIImageView!
+    @IBOutlet weak var phoneImg: UIImageView!
+    @IBOutlet weak var addressImg: UIImageView!
+    @IBOutlet weak var emailImg: UIImageView!
+    @IBOutlet weak var rentImg: UIImageView!
+    @IBOutlet weak var netImg: UIImageView!
+    @IBOutlet weak var gassImg: UIImageView!
+    @IBOutlet weak var khalaImg: UIImageView!
+    @IBOutlet weak var currentImg: UIImageView!
+    @IBOutlet weak var waterImg: UIImageView!
+    @IBOutlet weak var othersImg: UIImageView!
+    
+
+    
+    
+    
+    //MARK: - init
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .opaqueSeparator
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.view.backgroundColor = Utilities.color
+    }
+    
+    
+    
+    
+    //MARK: - Handler
+    
+    
+    
+
+
+}
